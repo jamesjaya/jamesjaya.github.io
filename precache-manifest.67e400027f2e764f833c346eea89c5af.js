5905413b45521e2769b0d6d6bee6119fd8d2737e
James Jaya <tjong.james@gmail.com>
2019-01-27 21:05:39 +0000

    Update title release

---
+++ precache-manifest.67e400027f2e764f833c346eea89c5af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c840fb164e6204d6f6eff94aba5b0ae",
+    "url": "/static/media/photo.6c840fb1.jpg"
+  },
+  {
+    "revision": "1751183466883584ad39659597b23498",
+    "url": "/static/media/bg-min.17511834.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31b69ec7f97df6610a0b",
+    "url": "/static/js/main.31b69ec7.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "31b69ec7f97df6610a0b",
+    "url": "/static/css/main.30443d0d.chunk.css"
+  },
+  {
+    "revision": "8b6a0f07fe42f53c252396e0532ba86a",
+    "url": "/index.html"
+  }
+];
